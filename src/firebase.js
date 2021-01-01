4c7b20a116f19b702132a945cdfb4d629e0bcc5e
nschuler <nicholas.schuler@student.uts.edu.au>
2021-01-01 22:53:33 +1100

    Adding FireBase

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
-
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messageSenderId: '',
-    apiId: ''
+    apiKey: "AIzaSyCb8-0i9-HwOMoVhCCpDkVw4dBWBQ7iMdw",
+    authDomain: "react-todoist-f7047.firebaseapp.com",
+    databaseURL: "https://react-todoist-f7047-default-rtdb.firebaseio.com",
+    projectId: "react-todoist-f7047",
+    storageBucket: "react-todoist-f7047.appspot.com",
+    messagingSenderId: "352887017453",
+    appId: "1:352887017453:web:402c8bac70149db13e21e4"
 })
 
 export { firebaseConfig as firebase }
