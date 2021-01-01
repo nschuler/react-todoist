8beaa44cfe90c6a73a2a611d41799e4f8a4def09
nschuler <nicholas.schuler@student.uts.edu.au>
2021-01-02 00:25:57 +1100

    Adding some context to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import { Content } from './components/layout/Content';
 import { Header } from './components/layout/Header';
+import { ProjectsProvider, SelectedProjectProvider } from './context';
 
 export const App = () => {
+
+
   return (
-    <div className="App">
-      <Header />
-      <Content />
-    </div>
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <div className="App">
+          <Header />
+          <Content />
+        </div>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
   );
 }
